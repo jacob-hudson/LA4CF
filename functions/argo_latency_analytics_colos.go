ed935b7c29670756c72dd649784f1444db4bb5b7
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2019-06-08 15:48:52 -0500

    colo latency

---
+++ functions/argo_latency_analytics_colos.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	// building the request URL
+	var prefix = "https://api.cloudflare.com/"
+	var path = "client/v4/zones/"
+	var zone = os.Getenv("CF_ZONE")
+	var suffix = "/analytics/latency/colos"
+
+	var request_url = prefix + path + zone + suffix
+
+	req, err := http.NewRequest("GET", request_url, nil)
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	req.Header.Set("X-Auth-Email", "jacob.alan.hudson@gmail.com")
+	req.Header.Set("X-Auth-Key", os.Getenv("CF_KEY"))
+
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+
+	fmt.Printf("%s\n", body)
+}
